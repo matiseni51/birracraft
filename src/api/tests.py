f921b121a101efdb848e9a0428c3a0aadaa1a2a9
matiseni51 <matiseni51@hotmail.com>
2022-08-29 21:18:46 +0200

    Feat: First test-create customer model- pytest

--- src/api/tests.py
+++ src/api/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.test import TransactionTestCase
 
-# Create your tests here.
+from api.models import *
+
+class TestCustomerModel(TransactionTestCase):
+   def test_create_customer(self):
+        name = 'John Johnson'
+        address = 'Irigoyen 800'
+        email = 'jj@gmail.com'
+        cellphone = '113974553311'
+        type = 'Comerce'
+        c = Customer.objects.create(
+            name=name,
+            address=address,
+            email=email,
+            cellphone=cellphone,
+            type=type,
+        )
+        self.assertTrue(Customer.objects.filter(id=c.id).exists())
+        self.assertEqual(c.name, name)
+        self.assertEqual(c.address, address)
+        self.assertEqual(c.email, email)
+        self.assertEqual(c.cellphone, cellphone)
+        self.assertEqual(c.type, type)
+        c.delete()
+        self.assertFalse(Customer.objects.filter(id=c.id).exists())
